37b50cc315027609b7feff300a453a0af5c966c7
ianEddie <ianeddie2@gmail.com>
2023-07-11 21:38:13 -0300

    index modificated

--- src/Components/ToggleMenu.jsx
+++ src/Components/ToggleMenu.jsx
@@ -3,8 +3,8 @@ import { menuIcon, closeIcon } from '../Data/images'
 //
 export function ToggleMenu ({ toggle, handleToggle }) {
   return (
-    <div className='absolute right-5 top-5'>
-      <img src={toggle ? closeIcon : menuIcon} alt='icon' onClick={handleToggle} className='cursor-pointer' />
+    <div className='absolute right-8 top-5'>
+      <img src={toggle ? closeIcon : menuIcon} alt='icon' onClick={handleToggle} className='cursor-pointer h-6' />
     </div>
   )
 }
